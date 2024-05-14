fe2576b53c7cef1eef35c5c21546e8ee2902448f
fredev <federico@aternum.dev>
2024-05-14 17:34:04 +0200

    feat(web3-utils): add addresses

---
+++ packages/web3-utils/src/address/index.ts
@@ -0,0 +1,17 @@
+export const arbitrumOne = {
+  lira: '0xA07ac236fEBc390c798504E927DC8D6a4e1FfcA3',
+  ldt: '0x2A5E22b32b3E0Daa7a8C199e10Df9D9E1264Fd3f'
+};
+
+export const arbitrumSepolia = {
+  lira: '0xC4868aA029ADD5705FA203580669d2175889D615',
+  ldt: '0x62F53E68662B013ea03B7BA6803b624632179eD3',
+  wbtc: '0xeD26d7763314A722A6A243057Bc76EF67C869D84',
+  weth: '0xdF5c1B370C7aE6C86d98A591C4aBe3453656a4b0',
+  factory: '0x92615C788f73C5C273ebE56EA7827529680f5281',
+  router: '0x92615C788f73C5C273ebE56EA7827529680f5281',
+};
+
+export const addresses = {
+  arbitrumSepolia,
+};
