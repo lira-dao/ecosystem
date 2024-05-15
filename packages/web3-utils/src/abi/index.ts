715c9e5d957ee330f94ac2f86bb262d873ada13f
fredev <federico@aternum.dev>
2024-05-15 18:22:07 +0200

    feat(web3-utils): add factory and pair abi

--- packages/web3-utils/src/abi/index.ts
+++ packages/web3-utils/src/abi/index.ts
@@ -1,3 +1,5 @@
 export { liraAbi } from './lira';
 export { liraDaoPresaleAbi } from './lira-dao-presale';
 export { baseFaucetAbi } from './base-faucet';
+export * from './dex-factory';
+export * from './dex-pair';
