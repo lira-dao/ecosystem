215129dc73a956f694f8d394d7f786e2fb0827e2
fredev <federico@aternum.dev>
2024-05-28 16:54:03 +0200

    feat: treasury

--- apps/dex/src/hooks/useTreasury.ts
+++ apps/dex/src/hooks/useTreasury.ts
@@ -1,5 +1,5 @@
 import { useAccount, useChainId, useReadContracts } from 'wagmi';
-import { erc20Abi } from '@lira-dao/web3-utils';
+import { treasuryTokenAbi } from '@lira-dao/web3-utils';
 import { getTreasuryCurrencies } from '../utils';
 import { formatUnits } from 'viem';
 import BigNumber from 'bignumber.js';
@@ -7,7 +7,22 @@ import BigNumber from 'bignumber.js';
 
 const balanceOf = {
   functionName: 'balanceOf',
-  abi: erc20Abi,
+  abi: treasuryTokenAbi,
+} as const;
+
+const mintFeeConfig = {
+  functionName: 'mintFee',
+  abi: treasuryTokenAbi,
+} as const;
+
+const burnFeeConfig = {
+  functionName: 'burnFee',
+  abi: treasuryTokenAbi,
+} as const;
+
+const rateConfig = {
+  functionName: 'rate',
+  abi: treasuryTokenAbi,
 } as const;
 
 export function useTreasury() {
@@ -19,11 +34,26 @@ export function useTreasury() {
 
   const balances = useReadContracts({ contracts: balancesContracts });
 
+  const mintFeeContracts = tokens.map(token => ({ ...mintFeeConfig, address: token.address }));
+
+  const mintFee = useReadContracts({ contracts: mintFeeContracts });
+
+  const burnContracts = tokens.map(token => ({ ...burnFeeConfig, address: token.address }));
+
+  const burnFee = useReadContracts({ contracts: burnContracts });
+
+  const rateContracts = tokens.map(token => ({ ...rateConfig, address: token.address }));
+
+  const rate = useReadContracts({ contracts: rateContracts });
+
   return tokens.map((token, i) => {
     return {
       ...token,
       balance: balances.data?.[i].result || 0n,
       formattedBalance: new BigNumber(formatUnits(balances.data?.[i].result || 0n, token.decimals)).decimalPlaces(6, 1).toString(),
+      mintFee: mintFee.data?.[i].result,
+      burnFee: burnFee.data?.[i].result,
+      rate: rate.data?.[i].result,
     };
   });
 }
--- apps/dex/src/pages/Treasury.tsx
+++ apps/dex/src/pages/Treasury.tsx
@@ -24,44 +24,56 @@ export function Treasury() {
           <x.div
             key={i}
             display="flex"
+            flexDirection="column"
             alignItems="center"
             justifyContent="space-between"
-            mb={i < treasuries.length - 1 ? 4 : 0}
-            pb={4}
+            mb={i < treasuries.length - 1 ? 8 : 0}
+            pb={8}
             borderBottom="1px solid"
             borderColor={th?.colors.gray155}
           >
-            <x.div display="flex" flexGrow={1} justifyContent="space-between" mr={4}>
-              <x.div>
-                <x.div display="flex" alignItems="center">
-                  <x.p fontSize="2xl" mr={2}>{treasury.name} ({treasury.symbol})</x.p>
-                  <x.img
-                    src={metamaskFox}
-                    alt="metamask icon"
-                    width={30}
-                    cursor="pointer"
-                    mr={2}
-                    onClick={() => addTreasuryToken(treasury)}
-                  />
-                </x.div>
-                <x.p fontSize="xl">BALANCE: {treasury.formattedBalance}</x.p>
-              </x.div>
+            <x.div w="100%" display="flex">
+              <x.p fontSize="2xl" mr={2}>{treasury.name} ({treasury.symbol})</x.p>
+              <x.img
+                src={metamaskFox}
+                alt="metamask icon"
+                width={30}
+                cursor="pointer"
+                mr={2}
+                onClick={() => addTreasuryToken(treasury)}
+              />
             </x.div>
 
-            <x.div display="flex" justifyContent="space-between">
-              <PrimaryButton
-                w="fit-content"
-                h="fit-content"
-                mr={2}
-                backgroundColor={th?.colors['green-yellow-700']}
-                onClick={() => navigate(`/treasury/${treasury.address}/mint`)}
-              >MINT</PrimaryButton>
-              <PrimaryButton
-                w="fit-content"
-                h="fit-content"
-                backgroundColor={th?.colors['red-700']}
-                onClick={() => navigate(`/treasury/${treasury.address}/burn`)}
-              >BURN</PrimaryButton>
+
+            <x.div w="100%" display="flex" flexDirection={{_: 'column', md: 'row'}} alignItems="center" justifyContent="space-between">
+              <x.div w="100%" display="flex" justifyContent="space-between" mr={{_: 0, md: 8}} my={{_: 8, md: 0}}>
+                <x.div>
+                  <x.p fontSize="xl" mb={2}>Balance: {treasury.formattedBalance} {treasury.symbol}</x.p>
+                  <x.p fontSize="xl">Rate: {treasury.rate?.toString()} {treasury.paired[0]}</x.p>
+                </x.div>
+
+                <x.div>
+                  <x.p fontSize="xl" mb={2}>Mint Fee: {treasury.mintFee?.toString()}%</x.p>
+                  <x.p fontSize="xl">Burn Fee: {treasury.burnFee?.toString()}%</x.p>
+                </x.div>
+              </x.div>
+
+              <x.div w={{_: '100%', md: 'fit-content'}} display="flex" flexDirection="column" justifyContent="space-between">
+                <PrimaryButton
+                  w={{ _: '100%', md: '180px' }}
+                  h="fit-content"
+                  mt={{ _: 4, md: 0 }}
+                  mb={2}
+                  backgroundColor={th?.colors['green-yellow-700']}
+                  onClick={() => navigate(`/treasury/${treasury.address}/mint`)}
+                >MINT</PrimaryButton>
+                <PrimaryButton
+                  w={{ _: '100%', md: '180px' }}
+                  h="fit-content"
+                  backgroundColor={{_: th?.colors['red-700'], hover: th?.colors['red-600']}}
+                  onClick={() => navigate(`/treasury/${treasury.address}/burn`)}
+                >BURN</PrimaryButton>
+              </x.div>
             </x.div>
           </x.div>
         ))}
