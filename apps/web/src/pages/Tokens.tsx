6e3f73d026238b138ea80b527c8307b055f57b1c
fredev <federico@aternum.dev>
2024-05-30 12:32:13 +0200

    feat(web): add treasury tokens

--- apps/web/src/pages/Tokens.tsx
+++ apps/web/src/pages/Tokens.tsx
@@ -1,5 +1,10 @@
 import styled, { ThemeProvider } from 'styled-components';
-import { addLiraDaoToken, addLiraToken } from '../utils/addLiraToken';
+import {
+  addLiraDaoToken,
+  addLiraToken,
+  addTreasuryBondBronzeToken, addTreasuryBondGoldToken,
+  addTreasuryBondSilverToken,
+} from '../utils/addLiraToken';
 import logoArbitrum from '../img/arbitrum-shield.png';
 import logoMetamask from '../img/metamask-fox.svg';
 import dexScreener from '../img/dex-screener.svg';
@@ -29,6 +34,9 @@ const StyledCopyButton = styled(Typography)`
 export function Tokens() {
   const copyLira = () => navigator.clipboard.writeText('0xA07ac236fEBc390c798504E927DC8D6a4e1FfcA3');
   const copyLdt = () => navigator.clipboard.writeText('0x2A5E22b32b3E0Daa7a8C199e10Df9D9E1264Fd3f');
+  const copyTbb = () => navigator.clipboard.writeText('0x9C0385b4F1f3B277ab352B817fC56763081a503c');
+  const copyTbs = () => navigator.clipboard.writeText('0x4bB0Eb07a8ECDcF5f434095Aa34Cc3f69292bcA1');
+  const copyTbg = () => navigator.clipboard.writeText('0xDB0aEb568EfE3598e9A58407c8b52BcFaC2c11e5');
 
   return (
     <ThemeProvider theme={theme}>
@@ -111,6 +119,153 @@ export function Tokens() {
             textAlign="center"
           >0x2A5E22b32b3E0Daa7a8C199e10Df9D9E1264Fd3f</Typography>
           <StyledCopyButton onClick={copyLdt} marginLeft={3}>
+            Copy
+          </StyledCopyButton>
+        </Row>
+
+        <Col my={40} />
+
+        <Row flexDirection={['column', 'column', 'column', 'row']} alignItems="center">
+          <Typography as="h5" color="white" margin="0 8px">Treasury Bond Bronze (TBb)</Typography>
+          <a
+            href="https://arbiscan.io/address/0x9C0385b4F1f3B277ab352B817fC56763081a503c"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <StyledLogo
+              src={logoArbitrum}
+              alt="link to treasury bond bronze token contract on arbiscan"
+              width={50}
+              height={50}
+            />
+          </a>
+          <a
+            href="https://dexscreener.com/arbitrum/0xAfb2aaA7b90905f32fDb3E61010F0dc2705827ca"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <StyledLogo
+              src={dexScreener}
+              alt="link to treasury bond bronze ldt pair on dextools"
+              width={50}
+              height={50}
+            />
+          </a>
+          <StyledLogo
+            src={logoMetamask}
+            alt="add to metamask"
+            onClick={addTreasuryBondBronzeToken}
+            width={50}
+            height={50}
+          />
+        </Row>
+        <Row flexDirection={['column', 'column', 'column', 'row']} alignItems="center">
+          <Typography
+            fontSize="body"
+            color="white"
+            wordBreak="break-all"
+            marginX={[20, 20, 20, 0]}
+            textAlign="center"
+          >0x9C0385b4F1f3B277ab352B817fC56763081a503c</Typography>
+          <StyledCopyButton onClick={copyTbb} marginLeft={3}>
+            Copy
+          </StyledCopyButton>
+        </Row>
+
+        <Col my={40} />
+
+        <Row flexDirection={['column', 'column', 'column', 'row']} alignItems="center">
+          <Typography as="h5" color="white" margin="0 8px">Treasury Bond Silver (TBs)</Typography>
+          <a
+            href="https://arbiscan.io/address/0x4bB0Eb07a8ECDcF5f434095Aa34Cc3f69292bcA1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <StyledLogo
+              src={logoArbitrum}
+              alt="link to treasury bond silver token contract on arbiscan"
+              width={50}
+              height={50}
+            />
+          </a>
+          <a
+            href="https://dexscreener.com/arbitrum/0xaFFEBBcbd0DD5AEDeCCdAB84b4103828780fD972"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <StyledLogo
+              src={dexScreener}
+              alt="link to treasury bond silver ldt pair on dextools"
+              width={50}
+              height={50}
+            />
+          </a>
+          <StyledLogo
+            src={logoMetamask}
+            alt="add to metamask"
+            onClick={addTreasuryBondSilverToken}
+            width={50}
+            height={50}
+          />
+        </Row>
+        <Row flexDirection={['column', 'column', 'column', 'row']} alignItems="center">
+          <Typography
+            fontSize="body"
+            color="white"
+            wordBreak="break-all"
+            marginX={[20, 20, 20, 0]}
+            textAlign="center"
+          >0x4bB0Eb07a8ECDcF5f434095Aa34Cc3f69292bcA1</Typography>
+          <StyledCopyButton onClick={copyTbs} marginLeft={3}>
+            Copy
+          </StyledCopyButton>
+        </Row>
+
+        <Col my={40} />
+
+        <Row flexDirection={['column', 'column', 'column', 'row']} alignItems="center">
+          <Typography as="h5" color="white" margin="0 8px">Treasury Bond Gold (TBg)</Typography>
+          <a
+            href="https://arbiscan.io/address/0xDB0aEb568EfE3598e9A58407c8b52BcFaC2c11e5"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <StyledLogo
+              src={logoArbitrum}
+              alt="link to treasury bond gold token contract on arbiscan"
+              width={50}
+              height={50}
+            />
+          </a>
+          <a
+            href="https://dexscreener.com/arbitrum/0xe24dB13D645218672D4D5Fc15f572328b32946A4"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <StyledLogo
+              src={dexScreener}
+              alt="link to treasury bond gold ldt pair on dextools"
+              width={50}
+              height={50}
+            />
+          </a>
+          <StyledLogo
+            src={logoMetamask}
+            alt="add to metamask"
+            onClick={addTreasuryBondGoldToken}
+            width={50}
+            height={50}
+          />
+        </Row>
+        <Row flexDirection={['column', 'column', 'column', 'row']} alignItems="center">
+          <Typography
+            fontSize="body"
+            color="white"
+            wordBreak="break-all"
+            marginX={[20, 20, 20, 0]}
+            textAlign="center"
+          >0xDB0aEb568EfE3598e9A58407c8b52BcFaC2c11e5</Typography>
+          <StyledCopyButton onClick={copyTbg} marginLeft={3}>
             Copy
           </StyledCopyButton>
         </Row>
--- apps/web/src/utils/addLiraToken.ts
+++ apps/web/src/utils/addLiraToken.ts
@@ -10,16 +10,15 @@ export async function addLiraToken() {
   const tokenImage = 'https://gateway.pinata.cloud/ipfs/QmeDcLiQAZ5VB2s3rzjjMYafU88tVrXLgEsC4Qon7bTaRM';
 
   try {
-    // wasAdded is a boolean. Like any RPC method, an error can be thrown.
     const wasAdded = await ethereum.request({
       method: 'wallet_watchAsset',
       params: {
-        type: 'ERC20', // Initially only supports ERC-20 tokens, but eventually more!
+        type: 'ERC20',
         options: {
-          address: tokenAddress, // The address of the token.
-          symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 characters.
-          decimals: tokenDecimals, // The number of decimals in the token.
-          image: tokenImage, // A string URL of the token logo.
+          address: tokenAddress,
+          symbol: tokenSymbol,
+          decimals: tokenDecimals,
+          image: tokenImage,
         },
       },
     });
@@ -41,16 +40,105 @@ export async function addLiraDaoToken() {
   const tokenImage = 'https://gateway.pinata.cloud/ipfs/QmQb1SsLMRdgUA1UPP5ZAn1ZryMifvFrKaUQGY1Mqdb3sy';
 
   try {
-    // wasAdded is a boolean. Like any RPC method, an error can be thrown.
     const wasAdded = await ethereum.request({
       method: 'wallet_watchAsset',
       params: {
-        type: 'ERC20', // Initially only supports ERC-20 tokens, but eventually more!
+        type: 'ERC20',
         options: {
-          address: tokenAddress, // The address of the token.
-          symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 characters.
-          decimals: tokenDecimals, // The number of decimals in the token.
-          image: tokenImage, // A string URL of the token logo.
+          address: tokenAddress,
+          symbol: tokenSymbol,
+          decimals: tokenDecimals,
+          image: tokenImage,
+        },
+      },
+    });
+
+    if (wasAdded) {
+      console.log('Thanks for your interest!');
+    } else {
+      console.log('Not added, maybe next time?');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function addTreasuryBondBronzeToken() {
+  const tokenAddress = '0x9C0385b4F1f3B277ab352B817fC56763081a503c';
+  const tokenSymbol = 'TBb';
+  const tokenDecimals = 18;
+  const tokenImage = 'https://gateway.pinata.cloud/ipfs/QmXdXZZL6ReTv5112xr88UhZHg4bj8TZqxFHiYu84WrmsR';
+
+  try {
+    const wasAdded = await ethereum.request({
+      method: 'wallet_watchAsset',
+      params: {
+        type: 'ERC20',
+        options: {
+          address: tokenAddress,
+          symbol: tokenSymbol,
+          decimals: tokenDecimals,
+          image: tokenImage,
+        },
+      },
+    });
+
+    if (wasAdded) {
+      console.log('Thanks for your interest!');
+    } else {
+      console.log('Not added, maybe next time?');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function addTreasuryBondSilverToken() {
+  const tokenAddress = '0x4bB0Eb07a8ECDcF5f434095Aa34Cc3f69292bcA1';
+  const tokenSymbol = 'TBs';
+  const tokenDecimals = 18;
+  const tokenImage = 'https://gateway.pinata.cloud/ipfs/QmXdXZZL6ReTv5112xr88UhZHg4bj8TZqxFHiYu84WrmsR';
+
+  try {
+    const wasAdded = await ethereum.request({
+      method: 'wallet_watchAsset',
+      params: {
+        type: 'ERC20',
+        options: {
+          address: tokenAddress,
+          symbol: tokenSymbol,
+          decimals: tokenDecimals,
+          image: tokenImage,
+        },
+      },
+    });
+
+    if (wasAdded) {
+      console.log('Thanks for your interest!');
+    } else {
+      console.log('Not added, maybe next time?');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function addTreasuryBondGoldToken() {
+  const tokenAddress = '0xDB0aEb568EfE3598e9A58407c8b52BcFaC2c11e5';
+  const tokenSymbol = 'TBg';
+  const tokenDecimals = 18;
+  const tokenImage = 'https://gateway.pinata.cloud/ipfs/QmXdXZZL6ReTv5112xr88UhZHg4bj8TZqxFHiYu84WrmsR';
+
+  try {
+    const wasAdded = await ethereum.request({
+      method: 'wallet_watchAsset',
+      params: {
+        type: 'ERC20',
+        options: {
+          address: tokenAddress,
+          symbol: tokenSymbol,
+          decimals: tokenDecimals,
+          image: tokenImage,
         },
       },
     });
