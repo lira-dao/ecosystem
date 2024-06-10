6cddc2f12f7e9c53427daa7ce9c2b25dd60822a3
nicholas <nicholas.angelucci@gmail.com>
2024-06-10 12:55:48 +0200

    feat: add mui, create theme refactor footer and header

--- apps/web/src/components/layout/MainLayout.tsx
+++ apps/web/src/components/layout/MainLayout.tsx
@@ -1,14 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from './Header';
-import { Footer } from './Footer';
+import { NewHeader } from './NewHeader';
+import { NewFooter } from './NewFooter';
 
 
 export function MainLayout() {
   return (
     <>
       <Header />
+      {/* <NewHeader /> */}
+
       <Outlet />
-      <Footer />
+      <NewFooter />
     </>
   );
 }
