4078dd9b296a1846b0c819377401e496c52c2c9f
fredev <federico@aternum.dev>
2024-06-12 00:05:56 +0200

    fix: farms table columns when disconnected

--- apps/dex/src/components/farming/FarmingTable.tsx
+++ apps/dex/src/components/farming/FarmingTable.tsx
@@ -20,11 +20,11 @@ export function FarmingTable({ farms, isConnected }: Props) {
           <TableHead>
             <TableRow>
               <TableCell>Farm</TableCell>
-              {isConnected && <TableCell align="right">Total Staked</TableCell>}
+              <TableCell align="right">Total Staked</TableCell>
               {isConnected && <TableCell align="right">LP Balance</TableCell>}
               {isConnected && <TableCell align="right">My Deposit</TableCell>}
               {isConnected && <TableCell align="right">Rewards</TableCell>}
-              <TableCell align="right">Actions</TableCell>
+              {isConnected && <TableCell align="right">Actions</TableCell>}
             </TableRow>
           </TableHead>
           <TableBody>
@@ -34,7 +34,7 @@ export function FarmingTable({ farms, isConnected }: Props) {
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
-                  <Box display="flex" alignItems="center" sx={{textWrap: 'nowrap'}}>
+                  <Box display="flex" alignItems="center" sx={{ textWrap: 'nowrap' }}>
                     <Box display="flex" mr={1}>
                       <img src={farm.tokens[0]?.icon} width={30} alt={`${farm.tokens[0]?.name} logo`} />
                       <img src={farm.tokens[1]?.icon} width={30} alt={`${farm.tokens[1]?.name} logo`} />
@@ -42,35 +42,37 @@ export function FarmingTable({ farms, isConnected }: Props) {
                     {farm.pair.symbol}
                   </Box>
                 </TableCell>
-                <TableCell align="right" sx={{textWrap: 'nowrap'}}>{farm.totalStaked} LP</TableCell>
-                {isConnected && <TableCell align="right" sx={{textWrap: 'nowrap'}}>{farm.balance} LP</TableCell>}
-                {isConnected && <TableCell align="right" sx={{textWrap: 'nowrap'}}>{farm.amount} LP</TableCell>}
+                <TableCell align="right" sx={{ textWrap: 'nowrap' }}>{farm.totalStaked} LP</TableCell>
+                {isConnected && <TableCell align="right" sx={{ textWrap: 'nowrap' }}>{farm.balance} LP</TableCell>}
+                {isConnected && <TableCell align="right" sx={{ textWrap: 'nowrap' }}>{farm.amount} LP</TableCell>}
                 {isConnected && (
-                  <TableCell align="right" sx={{textWrap: 'nowrap'}}>
+                  <TableCell align="right" sx={{ textWrap: 'nowrap' }}>
                     {farm.rewards[0]} {farm.tokens[0]?.symbol}<br />{farm.rewards[1]} {farm.tokens[1]?.symbol}
                   </TableCell>
                 )}
-                <TableCell align="right">
-                  <Box display="flex" justifyContent="end" alignItems="center">
-                    <Button
-                      color="success"
-                      variant="outlined"
-                      onClick={() => navigate(`/farming/${farm.address}/harvest`)}
-                      sx={{ marginRight: '10px' }}
-                    >Harvest</Button>
-                    <Button
-                      color="success"
-                      variant="outlined"
-                      onClick={() => navigate(`/farming/${farm.address}/stake`)}
-                      sx={{ marginRight: '10px' }}
-                    >Stake</Button>
-                    <Button
-                      color="error"
-                      variant="outlined"
-                      onClick={() => navigate(`/farming/${farm.address}/unstake`)}
-                    >Unstake</Button>
-                  </Box>
-                </TableCell>
+                {isConnected && (
+                  <TableCell align="right">
+                    <Box display="flex" justifyContent="end" alignItems="center">
+                      <Button
+                        color="success"
+                        variant="outlined"
+                        onClick={() => navigate(`/farming/${farm.address}/harvest`)}
+                        sx={{ marginRight: '10px' }}
+                      >Harvest</Button>
+                      <Button
+                        color="success"
+                        variant="outlined"
+                        onClick={() => navigate(`/farming/${farm.address}/stake`)}
+                        sx={{ marginRight: '10px' }}
+                      >Stake</Button>
+                      <Button
+                        color="error"
+                        variant="outlined"
+                        onClick={() => navigate(`/farming/${farm.address}/unstake`)}
+                      >Unstake</Button>
+                    </Box>
+                  </TableCell>
+                )}
               </TableRow>
             ))}
           </TableBody>
