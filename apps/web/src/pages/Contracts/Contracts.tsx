ed5b812a733ba5d532fbcc540ba7ec7a45e2063a
Nicholas Angelucci <nicholas.angelucci@gmail.com>
2024-06-11 12:01:18 +0200

    minor: ui

--- apps/web/src/pages/Contracts/Contracts.tsx
+++ apps/web/src/pages/Contracts/Contracts.tsx
@@ -12,19 +12,19 @@ export function Contracts() {
   return (
     <ThemeProvider theme={muiTheme}>
       <Container maxWidth="lg" sx={{ p: 4, display: 'flex', flexDirection: 'column', width: '100%' }}>
-        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', mb: 8 }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', mb: 4 }}>
           <Typography variant="h4" component="h1" color="white">Tokens</Typography>
         </Box>
 
         <TokensTable></TokensTable>
 
-        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', mt: 8, mb: 8 }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', mt: 8, mb: 4 }}>
           <Typography variant="h4" component="h1" color="white">Dex</Typography>
         </Box>
 
         <DexContractsTable></DexContractsTable>
 
-        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', mt: 8, mb: 8 }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', mt: 8, mb: 4 }}>
           <Typography variant="h4" component="h1" color="white">Pools</Typography>
         </Box>
 
