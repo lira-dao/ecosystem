a39108197f1233da546779f25f4f6513299c1150
fredev <federico@aternum.dev>
2024-06-13 19:42:27 +0200

    feat(web3-utils): add reward splitter address and abi

--- packages/web3-utils/tsup.config.ts
+++ packages/web3-utils/tsup.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig, type Options } from 'tsup';
 
 export default defineConfig((options: Options) => ({
-  entryPoints: ['src/index.ts'],
+  entry: ['src/index.ts'],
   clean: true,
   dts: true,
   format: ['cjs'],
+  onSuccess: 'mkdir -p dist/abi/json && cp -R src/abi/json dist/abi/',
   ...options,
 }));
