cfffd7a44b19315a85c382d16c33992155c60c79
fredev <federico@aternum.dev>
2024-06-14 20:52:21 +0200

    fix: farms decimals

--- apps/dex/src/hooks/useFarmingStakers.ts
+++ apps/dex/src/hooks/useFarmingStakers.ts
@@ -63,19 +63,19 @@ export function useFarmingStakers(): Farm[] {
       address: staker.address,
       pair: dexPairs[staker.pool],
       // @ts-ignore
-      amount: new BigNumber(amounts.data?.[i].result?.[0].toString()).div(new BigNumber(10).pow(18)).toFormat(6, 1) || '',
-      balance: new BigNumber(balances.data?.[i].result?.toString() || '0').div(new BigNumber(10).pow(18)).toFormat(6, 1) || '',
+      amount: new BigNumber(amounts.data?.[i].result?.[0].toString()).div(new BigNumber(10).pow(18)).toFormat(2, 1) || '',
+      balance: new BigNumber(balances.data?.[i].result?.toString() || '0').div(new BigNumber(10).pow(18)).toFormat(2, 1) || '',
       tokens: [
         getCurrencyByAddress(staker.tokens[0]),
         getCurrencyByAddress(staker.tokens[1]),
       ],
       rewards: [
         // @ts-ignore
-        new BigNumber(rewards.data?.[i].result?.[0].toString()).div(new BigNumber(10).pow(18)).toFormat(6, 1) || '',
+        new BigNumber(rewards.data?.[i].result?.[0].toString()).div(new BigNumber(10).pow(18)).toFormat(4, 1) || '',
         // @ts-ignore
-        new BigNumber(rewards.data?.[i].result?.[1].toString()).div(new BigNumber(10).pow(18)).toFormat(6, 1) || '',
+        new BigNumber(rewards.data?.[i].result?.[1].toString()).div(new BigNumber(10).pow(18)).toFormat(4, 1) || '',
       ],
-      totalStaked: new BigNumber(totalStaked.data?.[i].result?.toString() ?? '0').div(new BigNumber(10).pow(18)).toFormat(6) || '',
+      totalStaked: new BigNumber(totalStaked.data?.[i].result?.toString() ?? '0').div(new BigNumber(10).pow(18)).toFormat(2) || '',
     };
   });
 }
