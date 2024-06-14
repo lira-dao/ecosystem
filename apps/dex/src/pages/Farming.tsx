7ecd4c9d6d52fc47b6300f4bc987028715ba8281
fredev <federico@aternum.dev>
2024-06-14 20:52:41 +0200

    feat: move farms theme provider

--- apps/dex/src/pages/Farming.tsx
+++ apps/dex/src/pages/Farming.tsx
@@ -3,6 +3,9 @@ import { x } from '@xstyled/styled-components';
 import { useAccount } from 'wagmi';
 import { useFarmingStakers } from '../hooks/useFarmingStakers';
 import { FarmingTable } from '../components/farming/FarmingTable';
+import { ThemeProvider } from '@mui/material/styles';
+import { muiDarkTheme } from '../theme/theme';
+
 
 export function Farming() {
   const farms = useFarmingStakers();
@@ -10,14 +13,16 @@ export function Farming() {
 
 
   return (
-    <x.div display="flex" w="100%" flexDirection="column" maxWidth="1200px" p={4}>
-      <x.div display="flex" alignItems="center" justifyContent="space-between">
-        <x.h1 fontSize="4xl">Farming</x.h1>
-      </x.div>
+    <ThemeProvider theme={muiDarkTheme}>
+      <x.div display="flex" w="100%" flexDirection="column" maxWidth="1920px" p={4}>
+        <x.div display="flex" alignItems="center" justifyContent="space-between">
+          <x.h1 fontSize="4xl">Farming</x.h1>
+        </x.div>
 
-      <Box mt={8}>
-        <FarmingTable farms={farms} isConnected={isConnected} />
-      </Box>
-    </x.div>
+        <Box mt={8}>
+          <FarmingTable farms={farms} isConnected={isConnected} />
+        </Box>
+      </x.div>
+    </ThemeProvider>
   );
 }
