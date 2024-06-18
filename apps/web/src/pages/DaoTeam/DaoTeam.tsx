f830ef614538da998421096ac0ffad8195137da5
nicholas <nicholas.angelucci@gmail.com>
2024-06-18 18:52:03 +0200

    feat: dao team page UI

--- apps/web/src/pages/DaoTeam/DaoTeam.tsx
+++ apps/web/src/pages/DaoTeam/DaoTeam.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Avatar, Box, Card, CardContent, CardHeader, Container, Grid, Typography } from '@mui/material';
+import { Avatar, Box, Card, CardActions, CardContent, CardHeader, Container, Grid, IconButton, Typography } from '@mui/material';
 import { ThemeProvider, useTheme } from '@mui/material/styles';
 import muiTheme from '../../theme_mui';
 import avatarImage from '../../img/avatar_image.png';
-// import gradientLiraDex from '../../../img/gradient-lira-dex.svg';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 
 interface DaoTeamMember {
@@ -11,17 +11,63 @@ interface DaoTeamMember {
   role: string;
   description: string;
   avatarUrl: string;
+  linkedInUrl: string;
 }
 
 const members: DaoTeamMember[] = [
-  { name: 'John Doe', role: 'Developer', description: 'John is a leading developer with extensive blockchain experience.', avatarUrl: avatarImage },
-  { name: 'Jane Smith', role: 'Project Manager', description: 'Jane is responsible for project coordination and management.', avatarUrl: avatarImage },
-  { name: 'Rosbif', role: 'Developer', description: 'Rosbif is a leading developer with extensive blockchain experience.', avatarUrl: avatarImage },
+  {
+    name:"Erwin Roio",
+    role:"Co-founder",
+    description:"Specialist in tokenomics and smart contract logic since 2018, Bitcoin analyst.",
+    avatarUrl: avatarImage,
+    linkedInUrl: "https://www.linkedin.com/in/erwin-roio-49858b156/"
+  },
+  {
+    name:"Federico Paletta",
+    role:"Co-founder",
+    description:"Lead technical officer with extensive experience in the blockchain sector.",
+    avatarUrl: avatarImage,
+    linkedInUrl: "https://www.linkedin.com/in/federico-paletta-b1999a91/"
+  },
+{
+    name:"Jacopo Iessi",
+    role:"Co-founder",
+    description:"Head of official social media platforms.",
+    avatarUrl: avatarImage,
+    linkedInUrl: "https://www.linkedin.com/in/jacopo-iessi-666a9a19a/"
+  },
+  {
+    name:"Nicholas Angelucci",
+    role:"Co-founder",
+    description:"Expert developer in modern computing, including Artificial Intelligence and blockchain technology.",
+    avatarUrl: avatarImage,
+    linkedInUrl: "https://www.linkedin.com/in/nicholas-angelucci-65629616a/"
+  },
+  {
+    name:"Gabriele Passeri",
+    role:"Co-founder",
+    description:"Angel investor of the project, driven by a passion for making a difference in the community and fostering innovation.",
+    avatarUrl: avatarImage,
+    linkedInUrl: "https://www.linkedin.com/in/gabriele-passeri-701028218/"
+  }
 ];
 
 export function DaoTeam() {
   const DaoTeamCard = ({ member }: { member: DaoTeamMember }) => (
-    <Card sx={{ bgcolor: 'background.default', color: 'text.primary', m: 2, p: 0, display: 'flex', flexDirection: 'column', height: '100%' }}>
+    <Card sx={{
+      bgcolor: 'background.default',
+      color: 'text.primary',
+      m: 2,
+      p: 0,
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100%',
+      boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2)',
+      '&:hover': {
+        transform: 'scale(1.03)',
+        transition: 'transform 0.25s ease-in-out'
+      }
+    }}>
       <CardHeader
         avatar={<Avatar src={member.avatarUrl} aria-label="member-avatar" />}
         title={member.name}
@@ -34,6 +80,11 @@ export function DaoTeam() {
           {member.description}
         </Typography>
       </CardContent>
+      <CardActions disableSpacing sx={{ justifyContent: 'flex-end', p: 2 }}>
+        <IconButton href={member.linkedInUrl} target="_blank" sx={{ color: 'primary.main' }}>
+          <LinkedInIcon />
+        </IconButton>
+      </CardActions>
     </Card>
   );
 
@@ -41,7 +92,14 @@ export function DaoTeam() {
     <ThemeProvider theme={muiTheme}>
       <Container maxWidth="lg" sx={{ p: 4, display: 'flex', flexDirection: 'column', width: '100%' }}>
         <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', mb: 4 }}>
-          <Typography variant="h4" component="h1" color="white">Dao Team</Typography>
+          <Typography variant="h4" component="h1" color="white">Lira Dao Team</Typography>
+        </Box>
+
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', mb: 4 }}>
+          <Typography variant="xl" textAlign="justify" margin={0}>
+            Meet Our Team
+            We are proud to introduce the talented and diverse team behind our project. Our co-founders bring a wealth of experience and a shared passion for innovation and community impact. Each member of our team plays a critical role in driving our mission forward and ensuring the success of our initiatives. Get to know the individuals who are making it all happen:
+          </Typography>
         </Box>
 
         <Grid container spacing={3}>
