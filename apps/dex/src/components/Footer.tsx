e7c71097897e030b0cd9dd20965c2f07e2dcc0f4
Nicholas Angelucci <nicholas.angelucci@gmail.com>
2024-07-08 16:12:58 +0200

    feat: footer, coinmarketcap price

--- apps/dex/src/components/Footer.tsx
+++ apps/dex/src/components/Footer.tsx
@@ -1,38 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Grid, Link } from '@mui/material';
-
+import { useTheme, x } from '@xstyled/styled-components';
 import Typography from "@mui/material/Typography";
 import { ThemeProvider } from '@mui/material/styles';
 import { muiDarkTheme } from '../theme/theme';
 
 
 export function Footer() {
+  const th = useTheme();
+  const [hover, setHover] = useState(false);
+
   return (
     <ThemeProvider theme={muiDarkTheme}>
       <Box 
         sx={{
-          // margin: '20px 20px 0',
-          marginTop: '40px',
+          position: 'sticky',
+          bottom: 0,
+          width: '100%',
           display: 'flex',
           flexDirection: 'column',
-        }}
-      >
-        <Grid container spacing={3} justifyContent="space-between">
+          alignItems: 'flex-end',
+          padding: '20px',
+          backgroundColor: 'background.paper',
+        }}>
+        <Grid container spacing={3} justifyContent="flex-end">
           <Grid item>
-            <Typography variant="body2" color="common.white" gutterBottom>
-              Powered by 
+            <Typography variant="body2" color={th?.colors.gray155} gutterBottom>
+              Prices by 
             </Typography>
-            <Link href="https://coinmarketcap.com" target="_blank" rel="noopener noreferrer">
-              <Typography variant="subtitle1" color="common.white">
+            <Link href="https://coinmarketcap.com" target="_blank" rel="noopener noreferrer" 
+              onMouseEnter={() => setHover(true)}
+              onMouseLeave={() => setHover(false)}
+              sx={{
+                textDecoration: 'none',
+                color: hover ? th?.colors.white + '!important' : th?.colors.gray155 + '!important'
+              }}>
+              <Typography variant="subtitle1" sx={{ 
+                fontSize: '0.8rem',
+                textDecoration: 'none',
+              }}>
                 CoinMarketCap
               </Typography>
             </Link>
           </Grid>
         </Grid>
-
-        {/* <Typography variant="body2" color="textSecondary" align="center" sx={{ mt: 4 }}>
-          © {new Date().getFullYear()} Your Company Name. All rights reserved.
-        </Typography> */}
       </Box>
     </ThemeProvider>
   );
