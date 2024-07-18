eb9e2ca5d159e410ed57520da26648cad5edc10a
fredev <federico@aternum.dev>
2024-07-18 14:47:23 +0200

    fix: add view to rate in treasury token interface

--- contracts/treasury-tokens/contracts/interfaces/ITreasuryToken.sol
+++ contracts/treasury-tokens/contracts/interfaces/ITreasuryToken.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.24;
 
 interface ITreasuryToken {
     function mint(address to, uint256 amount) external;
-    function rate() external returns (uint);
+    function rate() external view returns (uint);
     function quoteMint(uint256 amount) external view returns (uint256 total, uint256 cost, uint256 fee);
 }
