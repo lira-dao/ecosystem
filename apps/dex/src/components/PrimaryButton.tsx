35e18ad2c6d54fe0037fc604c623ca2ba24ea40d
fredev <federico@aternum.dev>
2024-07-19 15:03:12 +0200

    feat: refactor primary button

--- apps/dex/src/components/PrimaryButton.tsx
+++ apps/dex/src/components/PrimaryButton.tsx
@@ -1,60 +1,16 @@
-import { styled, th } from '@xstyled/styled-components';
+import { styled } from '@mui/material/styles';
+import Button, { ButtonProps } from '@mui/material/Button';
+import { green } from '../theme/theme';
 
 
-export const PrimaryButton = styled.buttonBox`
-  width: 100%;
-  background-color: green-yellow-600;
-  font-size: 20px;
-  font-weight: 535;
-  padding: 16px;
-  color: ${th.color('white')};
-
-  align-items: center;
-  border-radius: 16px;
-  border: 1px solid transparent;
-  cursor: pointer;
-  display: flex;
-  flex-wrap: nowrap;
-  justify-content: center;
-  line-height: 24px;
-  outline: none;
-  position: relative;
-  text-align: center;
-  text-decoration: none;
-  z-index: 1;
-  
-  &:focus {
-    box-shadow: 0 0 0 1pt green-yellow-600;
-    background-color: green-yellow-600;
-    outline: none;
+export const PrimaryButton = styled(Button)<ButtonProps>(({ theme }) => ({
+  color: theme.palette.getContrastText(green[600]),
+  fontSize: theme.typography.h6.fontSize,
+  backgroundColor: green[600],
+  '&:hover': {
+    backgroundColor: green[500],
+  },
+  '&:focus': {
+    outline: 'none',
   }
-  &:hover {
-    background-color: green-yellow-600;
-    color: white;
-  }
-  &:active {
-    box-shadow: 0 0 0 1pt green-yellow-700;
-    background-color: green-yellow-700;
-  }
-  &:disabled {
-    background-color: gray94;
-    color: gray155;
-    cursor: auto;
-    box-shadow: none;
-    border: 1px solid transparent;
-    outline: none;
-  }
-
-  will-change: transform;
-  transition: transform 450ms ease;
-  transition: background-color 450ms ease;
-  transform: perspective(1px) translateZ(0);
-
-  > * {
-    user-select: none;
-  }
-
-  > a {
-    text-decoration: none;
-  }
-`;
+}));
