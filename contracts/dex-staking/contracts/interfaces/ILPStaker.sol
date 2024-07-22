8cf8c1d520a06fed7df811e844dbf83ca5d2a0ec
fredev <federico@aternum.dev>
2024-07-22 10:29:58 +0200

    chore: clean code

--- contracts/dex-staking/contracts/interfaces/ILPStaker.sol
+++ contracts/dex-staking/contracts/interfaces/ILPStaker.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.24;
 
 interface ILPStaker {
     function totalStaked() external view returns (uint256);
-
     function lpToken() external view returns (address);
-
     function distributeRewards(uint256 rewardAmount1, uint256 rewardAmount2) external;
 }
