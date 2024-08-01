c6598d26185c647b0ad80beaf5198c5df9298b8f
fredev <federico@aternum.dev>
2024-08-01 14:36:28 +0200

    Issues/6 (#50)
    * Added social icons to footer
    
    * chore: resolve conflict
    
    ---------
    
    Co-authored-by: dev0614 <shimamuratakehiko44@gmail.com>

--- apps/dex/src/components/Footer.tsx
+++ apps/dex/src/components/Footer.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
-import { Box, Link } from '@mui/material';
+import { Box, Container, Grid, Link } from '@mui/material';
 import { useTheme } from '@xstyled/styled-components';
 import Typography from '@mui/material/Typography';
 import { ThemeProvider } from '@mui/material/styles';
 import { muiDarkTheme } from '../theme/theme';
+import twitter from '../img/twitter.png';
+import discord from '../img/discord.svg';
+import telegram from '../img/telegram.png';
 
 
 export function Footer() {
@@ -23,21 +26,91 @@ export function Footer() {
           px: 4,
         }}
       >
-        <Typography variant="body2" color="text.secondary" noWrap>
-          Prices by
-          <Link
-            href="https://coinmarketcap.com" target="_blank" rel="noopener noreferrer"
-            onMouseEnter={() => setHover(true)}
-            onMouseLeave={() => setHover(false)}
-            sx={{
-              color: hover ? th?.colors.white + '!important' : th?.colors.gray155 + '!important',
-              textDecoration: 'none',
-              ml: 1,
-            }}
+        <Container>
+          <Grid
+            container
+            display="flex"
+            justifyContent={'space-between'}
+            alignItems="center"
           >
-            CoinMarketCap
-          </Link>
-        </Typography>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: {
+                  xs: 2,
+                  sm: 3,
+                  md: 4,
+                },
+              }}
+            >
+              <Link
+                href="https://twitter.com/LIRA_DAO"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Box
+                  component="img"
+                  src={twitter}
+                  alt="Twitter"
+                  sx={{
+                    height: { xs: 20, md: 24 },
+                    width: 'auto',
+                  }}
+                />
+              </Link>
+              <Link
+                href="https://discord.gg/fDRBajCB9V"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Box
+                  component="img"
+                  src={discord}
+                  alt="Discord"
+                  sx={{
+                    height: { xs: 16, md: 20 },
+                    width: 'auto',
+                  }}
+                />
+              </Link>
+              <Link
+                href="https://t.me/LIRA_DAO"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Box
+                  component="img"
+                  src={telegram}
+                  alt="Telegram"
+                  sx={{
+                    height: { xs: 20, md: 24 },
+                    width: 'auto',
+                  }}
+                />
+              </Link>
+            </Box>
+            <Typography variant="body2" color="text.secondary" noWrap>
+              Prices by
+              <Link
+                href="https://coinmarketcap.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}
+                sx={{
+                  color: hover
+                    ? th?.colors.white + '!important'
+                    : th?.colors.gray155 + '!important',
+                  textDecoration: 'none',
+                  ml: 1,
+                }}
+              >
+                CoinMarketCap
+              </Link>
+            </Typography>
+          </Grid>
+        </Container>
       </Box>
     </ThemeProvider>
   );
